a1e81d8e6c9e73583e10e3ec8f7ada524f1a285f
Fabiano Cunha <cunhasb@aol.com>
2017-11-24 22:25:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers){
+  return drivers.slice().forEach(function(element){
+    console.log(element.name);
+  })
+}
+
+function logDriversByHometown(drivers,hometown){
+  return drivers.slice().forEach(function(el){
+    if (el.hometown === hometown){
+      console.log(el.name);
+    }
+  })
+}
+
+function driversByRevenue(drivers){
+  return drivers.slice().sort(function(a,b){
+    return (a.revenue - b.revenue);
+  })
+}
+
+function driversByName(drivers){
+  return drivers.slice().sort(function(a,b){
+    return a.name.localeCompare(b.name);
+  })
+  }
+
+  function totalRevenue(drivers){
+    return drivers.slice().reduce(function(total,el){
+      return total + el.revenue
+    },0);
+  }
+
+  function averageRevenue(drivers){
+    results =  drivers.slice().reduce(function(total,el,index){
+      return (total + el.revenue);
+    },0)
+    return (results / drivers.length);
+  }
